deadd1008c72eca10eb7ee1a1d2b131699fcebe6
invakid404 <invakid404@riseup.net>
2022-09-18 15:55:41 +0300

    chore(bootstrap): create sops secret

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+kubectl create namespace flux-system
+
+export SOPS_PGP_FP=$(cat .sops.yaml | yq -r '.creation_rules[0].pgp')
+
+gpg --export-secret-keys --armor "${SOPS_PGP_FP}" |
+	kubectl create secret generic sops-gpg \
+	--namespace=flux-system \
+	--from-file=sops.asc=/dev/stdin
+
 flux bootstrap github \
   --owner=invakid404 \
   --repository=home-cluster \
