dead8fa885e8c95a2cbb6b7c0f3e65b715e6c750
invakid404 <invakid404@riseup.net>
2022-12-31 14:56:00 +0200

    chore(install): disable klipper-lb

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ IP=$(ifconfig wlan0 | awk '/inet / {print $2}')
 curl -sfL https://get.k3s.io | \
 	INSTALL_K3S_EXEC="--disable helm-controller \
 		--disable traefik \
+		--disable servicelb \
 		--bind-address ${IP} \
 		--node-ip ${IP} \
 		--advertise-address ${IP} \
